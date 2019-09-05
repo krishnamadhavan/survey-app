b83d182f6c798c3a4e7d0d142e8d9732463a6413
Krishna Madhavan <krishnamadhavan07@gmail.com>
2019-09-06 00:56:09 +0530

    Commit with whitenoise v4.0 upgrades done.

--- survey_app/settings.py
+++ survey_app/settings.py
@@ -155,7 +155,7 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Use Database URLs in your Django Application.
